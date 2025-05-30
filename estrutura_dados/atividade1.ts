4da4ed197060b0d88663ae41c19d15565270e729
Gih <giovannabreinackcolombara@gmail.com>
2025-05-30 14:51:48 -0300

    atividade 2

--- estrutura_dados/atividade1.ts
+++ estrutura_dados/atividade1.ts
@@ -1,19 +1,13 @@
 import leia from 'readline-sync'
 import { Queue } from "./Queue";
-
 const fila = new Queue<string>();
-
 let opcao;
 let nome: string;
-
 do{
 console.log("******************************" + "\n" + "1 - Adicionar Cliente na Fila" + "\n" + "2 - Listar todos os Clientes"
     + "\n" + "3 - Retirar Cliente da Fila" + "\n" + "0 - Sair" + "\n" + "******************************"
 )
-
 opcao = leia.questionInt("Digite uma opcao:")
-
-
 switch(opcao){
     case 0:
         console.log("Ate logo!");
